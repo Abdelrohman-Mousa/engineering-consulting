66cdb83891696fc6ca75694a34db9082672137d0
Abdelrohman-Mousa <abdelrohmanmarey@gmail.com>
2025-11-09 09:40:14 +0200

    to update

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+// import { reactRouter } from "@react-router/dev/vite";
+// import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+// import tsconfigPaths from "vite-tsconfig-paths";
+//
+// export default defineConfig({
+//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+// });
+
+
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -5,4 +15,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  optimizeDeps: {
+    include: ['rough-notation'] // يجبر Vite على تحويل rough-notation
+  },
+  ssr: {
+    noExternal: ['rough-notation'], // لو بتعمل SSR أو Deploy على Vercel
+  }
 });
